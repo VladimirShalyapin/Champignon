172c098816e53c7cfcb1388c9f3c1937ddcc51dd
Vladimir Shalyapin <91007213+VladimirShalyapin@users.noreply.github.com>
2022-05-14 13:04:03 +0300

    Update data_preparation_and_photo_processing.py

--- RUS/data_preparation_and_photo_processing.py
+++ RUS/data_preparation_and_photo_processing.py
@@ -11,8 +11,11 @@ def gray_image(x):
     na_image_gray = np.around(image_gray, decimals = 3)
     return na_image_gray, image_gray
 
+# облегчение работы с единичными файлами путем оптимизации ввода названия файла
+name_file = str('11')
+
 # в функцию размещается обрабатываемое изображение 
-na_image_gray, image_gray = gray_image('name.JPG')
+na_image_gray, image_gray = gray_image(name_file + ".JPG")
 
 # формирует массив данных (1, 192) с интревалом занчений от 0.01 до 0.99
 r_na_image_gray = (na_image_gray.reshape(1, 192) / 1.02) + 0.01
@@ -20,15 +23,15 @@ r_na_image_gray = (na_image_gray.reshape(1, 192) / 1.02) + 0.01
 # округляем значения в массиве до сотых
 data = np.around(r_na_image_gray, decimals = 2)
 
-# вводим целевую переменную в наши данных np.insert(np.append(data, []), 'позиция т.е первая'0, 'целевая и известная переменная, она же номер группы'7)
-target_data = np.insert(np.append(data, []), 0, 7)
+# вводим целевую переменную в наши данных np.insert(np.append(data, []), 'позиция т.е первая'0, 'целевая и известная переменная, она же номер группы'0)
+target_data = np.insert(np.append(data, []), 0, 0)
 
 # формируем лист (1, 193) для сохранения его в .csv файл
 r_target_data = target_data.reshape(1, 193)
 print(r_target_data.shape)
 
 # сохраняем полученные значения и смотрим что получилось 
-save_target_data = np.savetxt("target_data.csv", r_target_data, delimiter=",", newline='\n', encoding='ANSI')
-load_target_data = np.loadtxt("target_data.csv", delimiter=",")
+save_target_data = np.savetxt(name_file + ".csv", r_target_data, delimiter=",", newline='\n', encoding='ANSI')
+load_target_data = np.loadtxt(name_file + ".csv", delimiter=",")
 print(load_target_data)
 print(load_target_data.shape)
