cbf0a142d4fc70e0e0ee5172eb3ed3f640b3c249
Vladimir Shalyapin <91007213+VladimirShalyapin@users.noreply.github.com>
2022-05-26 16:01:01 +0300

    Create size_formatting.py

---
+++ V1/RUS/size_formatting.py
@@ -0,0 +1,25 @@
+###########################################################
+########### MIT license 2022 Vladimir Shalyapin ###########
+###########            Python   v 3.8           ###########
+###########################################################
+ 
+# импорт необходимых библиотек
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+ 
+# загружаем файл и его размер
+image = imread('namefile.JPG')
+print(image.shape)
+ 
+# визуализация исходного изображения
+plt.imshow(image)
+plt.show()
+ 
+# изменение размеров до необходимого 16*12 px, по средствам изменения // 77 и // 124
+image_resized = resize(image, (image.shape[0] // 77, image.shape[1] // 124), anti_aliasing=True)
+ 
+# визуализация полученного изображения
+print(image_resized.shape)
+plt.imshow(image_resized )
+plt.show()
